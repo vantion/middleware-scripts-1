64a18ed5fe5deb7ae67d5cff895c730290283534
Ted <teddygadie@gmail.com>
2022-03-11 06:05:05 -0500

    indentation adjusted

--- jenkinsInstall.sh
+++ jenkinsInstall.sh
@@ -5,50 +5,41 @@
 
 user=`whoami`
 if [ $user != root ]
-	   then
-		      echo -e "\n You need root access to run this installation.\n"
-		         exit 2
+   then
+   echo -e "\n You need root access to run this installation.\n"
+   exit 2
 fi
 
 echo -e  "\n Jenkins installation in process...\n "
 sleep 1
 
-    # Installations 
-    yum install java-1.8.0-openjdk-devel wget -y
-    if [ $? -ne 0 ]
-	       then
-		          echo -e "\n System error installing packages \n"
-			     exit 3
-    fi
+  # Installations 
+yum install java-1.8.0-openjdk-devel wget jenkins -y
+if [ $? -ne 0 ]
+   then
+   echo -e "\n System error installing dependencies \n"
+   exit 3
+fi
 
-    sleep 2
-    wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo 
-    sleep 2
-    sed -i 's/gpgcheck=1/gpgcheck=0/g' /etc/yum.repos.d/jenkins.repo
+sleep 2
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo       
+sudo sed -i 's/gpgcheck=1/gpgcheck=0/g' /etc/yum.repos.d/jenkins.repo
 
-    sleep 2
+sleep 2
+systemctl start jenkins
+systemctl enable jenkins
 
-    yum install jenkins -y
+  # Status of the jenkins Daemon
+systemctl status jenkins | grep active |awk -F ":" '{print$2$3$4$5}'
 
-    sleep 1
-        # Activation of the jenkins Daemon service
-	systemctl start jenkins
-	systemctl enable jenkins
-	systemctl status jenkins | grep active |awk -F ":" '{print$2$3$4$5}'
+  # Open port 8080
+sleep 1
+sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
+sudo firewall-cmd --reload
 
-	    # Opening port 8080
-	    sleep 1 
-	    firewall-cmd --permanent --zone=public --add-port=8080/tcp
-	    firewall-cmd --reload
+  # Open the browser and test the ip address
 
-	        # Open the browser and test the ip address 
-		echo -e "\n The Jenkins Installation was successful, follow the link below... \n"
-		sleep 1
-		ipaddr=`hostname -I |awk '{print$2}'`
-		echo -e  "\n http://$ipaddr:8080 \n" 
-
-		# Unlock Jenkins on the browser :
-
-		echo "Please copy the password below to unlock Jenkins on the browser:"
-		Password=`cat /var/lib/jenkins/secrets/initialAdminPassword`
-		echo -e "\n$Password\n"
+echo -e "\n The Jenkins Installation was successful, follow the link below... \n"
+sleep 1
+ipaddr=`hostname -I |awk '{print$2}'`
+echo -e  "\n http://$ipaddr:8080 \n"
